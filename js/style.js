4dba9e8ad6f8ba590434494aff2983c1c2dd688a
khushal <kmakwana8232@gamil.com>
2023-12-26 18:07:50 +0530

    add all

---
+++ js/style.js
@@ -0,0 +1,37 @@
+$('#carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:2
+        }
+    }
+})
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    center:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+ 
